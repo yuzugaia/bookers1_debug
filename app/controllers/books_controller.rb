fad99c4ebbed31d09e80cddef048519455a7dfb7
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-21 11:39:21 +0000

    [fix]bookers1 error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
@@ -27,8 +28,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
-      flash[:notice] = "Book was successfully updated."
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(@book.id)
     else
       render :edit
